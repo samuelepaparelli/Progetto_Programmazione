227b0e5330dede504b070b4f418e7287af33ec55
samuelepaparelli <75261729+samuelepaparelli@users.noreply.github.com>
2020-12-14 17:29:11 +0100

    Add files via upload
    progetto con spring boot

---
+++ Progetto_POO/src/main/java/com/esame/Progetto_POO/ProgettoPooApplication.java
@@ -0,0 +1,13 @@
+package com.esame.Progetto_POO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoPooApplication.class, args);
+	}
+
+}
---
+++ Progetto_POO/src/test/java/com/esame/Progetto_POO/ProgettoPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esame.Progetto_POO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
